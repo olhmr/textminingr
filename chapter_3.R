a5f16cde86fcb5c8e3eb88ce23c61e02ed799e41
Olle Hammarström <olle.hammarstrom@gmail.com>
2019-05-11 13:35:00 +0100

    Finished chapter 3

--- chapter_3.R
+++ chapter_3.R
@@ -73,3 +73,71 @@ book_words %>%
   labs(x = NULL, y = "tf-idf") +
   facet_wrap(~book, ncol = 2, scales = "free") +
   coord_flip()
+
+library(gutenbergr)
+physics <- gutenberg_download(c(37729, 14725, 13476, 5001), 
+                              meta_fields = "author") # Download of 5001 (Einstein) failed
+
+physics_words <- physics %>%
+  unnest_tokens(word, text) %>%
+  count(author, word, sort = TRUE)
+physics_words
+
+plot_physics <- physics_words %>%
+  bind_tf_idf(word, author, n) %>%
+  arrange(desc(tf_idf)) %>%
+  mutate(word = factor(word, levels = rev(unique(word)))) %>%
+  mutate(author = factor(author, levels = c("Galilei, Galileo",
+                                            "Huygens, Christiaan",
+                                            "Tesla, Nikola",
+                                            "Einstein, Albert")))
+plot_physics %>%
+  group_by(author) %>%
+  top_n(15, tf_idf) %>%
+  ungroup() %>%
+  mutate(word = reorder(word, tf_idf)) %>% # Not sure this actually does anything
+  # The list should already be sorted by arrange call when we created plot_physics
+  ggplot(aes(x = word, y = tf_idf, fill = author)) +
+  geom_col(show.legend = FALSE) +
+  labs(x = NULL, y = "tf-idf") +
+  facet_wrap(~author, ncol = 2, scales = "free") + 
+  coord_flip()
+# Galilei should probably not have `equall` that high up, since it's just an
+# alternate spelling of equal
+
+library(stringr)
+physics %>%
+  filter(grepl(x = text, pattern = "eq\\.")) %>% # Obviously won't find anything when we failed to download Einstein
+  # Why use str_detect from stringr When grepl is in base and works just as
+  # well?
+  select(text)
+
+physics %>%
+  filter(grepl(x = text, pattern = "K1")) %>%
+  select(text)
+
+physics %>%
+  filter(grepl(pattern = "AK", x = text)) %>%
+  select(text)
+
+mystopwords <- tibble(word = c("eq", "co", "rc", "ac", "ak", "bn", 
+                               "fig", "file", "cg", "cb", "cm"))
+physics_words <- physics_words %>%
+  anti_join(mystopwords, by = "word")
+plot_physics <- physics_words %>% # Same as before
+  bind_tf_idf(word, author, n) %>%
+  arrange(desc(tf_idf)) %>%
+  mutate(word = factor(word, levels = rev(unique(word)))) %>%
+  group_by(author) %>% 
+  top_n(15, tf_idf) %>%
+  ungroup() %>%
+  mutate(author = factor(author, levels = c("Galilei, Galileo",
+                                            "Huygens, Christiaan",
+                                            "Tesla, Nikola",
+                                            "Einstein, Albert")))
+ggplot(data = plot_physics, 
+       aes(x = word, y = tf_idf, fill = author)) +
+  geom_col(show.legend = FALSE) +
+  labs(x = NULL, y = "tf-idf") +
+  facet_wrap(~author, ncol = 2, scales = "free") +
+  coord_flip()
