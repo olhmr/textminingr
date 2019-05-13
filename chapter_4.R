fad2d85ac9461a6c2214fdcec7281bd197c86fc9
Olle Hammarström <olle.hammarstrom@gmail.com>
2019-05-13 21:04:02 +0100

    Finished 4.1.4

--- chapter_4.R
+++ chapter_4.R
@@ -86,3 +86,26 @@ negated_words <- bigrams_separated %>%
   filter(word1 %in% negation_words) %>%
   inner_join(AFINN, by = c(word2 = "word")) %>%
   count(word1, word2, score, sort = TRUE)
+
+library(igraph)
+bigram_counts
+bigram_graph <- bigram_counts %>%
+  filter(n > 20) %>%
+  graph_from_data_frame()
+bigram_graph
+
+library(ggraph)
+set.seed(2017)
+ggraph(bigram_graph, layout = "fr") + # Unclear from help page what layout "fr" is
+  geom_edge_link() +
+  geom_node_point() +
+  geom_node_text(aes(label = name), vjust = 1, hjust = 1)
+
+set.seed(2016)
+a <- grid::arrow(type = "closed", length = unit(0.15, "inches"))
+ggraph(bigram_graph, layout = "fr") +
+  geom_edge_link(aes(edge_alpha = n), show.legend = FALSE,
+                 arrow = a, end_cap = circle(0.07, "inches")) +
+  geom_node_point(color = "lightblue", size = 5) +
+  geom_node_text(aes(label = name), vjust = 1, hjust = 1) + 
+  theme_void()
