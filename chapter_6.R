3149b4f1d4aacae87571b13ec61b6b20b5b4ac56
Olle Hammarström <olle.hammarstrom@gmail.com>
2019-05-23 21:27:04 +0100

    Finished 6.1.1

--- chapter_6.R
+++ chapter_6.R
@@ -6,3 +6,30 @@ AssociatedPress
 
 ap_lda <- LDA(AssociatedPress, k = 2, control = list(seed = 1234))
 ap_lda
+
+library(tidytext)
+ap_topics <- tidy(ap_lda, matrix = "beta") 
+# Beta is the per-topic-per-word probabilities
+ap_topics
+
+library(ggplot2)
+library(dplyr)
+ap_top_terms <- ap_topics %>%
+  group_by(topic) %>%
+  top_n(10, beta) %>%
+  ungroup() %>%
+  arrange(topic, -beta)
+ap_top_terms %>%
+  mutate(term = reorder(term, beta)) %>%
+  ggplot(aes(x = term, y = beta, fill = factor(topic))) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~topic, scales = "free") +
+  coord_flip()
+
+library(tidyr)
+beta_spread <- ap_topics %>%
+  mutate(topic = paste0("topic", topic)) %>%
+  spread(topic, beta) %>%
+  filter(topic1 > 0.001 | topic2 > 0.001) %>%
+  mutate(log_ratio = log2(topic2 / topic1))
+beta_spread
