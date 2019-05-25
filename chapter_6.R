3a3ef0ad3a2a636f2c86aab42ee6f4002a37176f
Olle Hammarström <olle.hammarstrom@gmail.com>
2019-05-25 22:04:01 +0100

    Finished 6.2.0

--- chapter_6.R
+++ chapter_6.R
@@ -40,3 +40,24 @@ ap_documents
 tidy(AssociatedPress) %>%
   filter(document == 6) %>%
   arrange(desc(count))
+
+titles <- c("Twenty Thousand Leagues under the Sea", "The War of the Worlds", 
+            "Pride and Prejudice", "Great Expectations")
+library(gutenbergr)
+books <- gutenberg_works(title %in% titles) %>% # Find info on the titles in the database
+  gutenberg_download(meta_fields = "title") # Search based on found info
+
+library(stringr)
+by_chapter <- books %>%
+  group_by(title) %>%
+  mutate(chapter = cumsum(str_detect(text, regex("^chapter ", ignore_case = TRUE)))) %>%
+  ungroup() %>%
+  filter(chapter > 0) %>%
+  unite(document, title, chapter) # Join title and chapter into column document, separating with _ by default
+by_chapter_word <- by_chapter %>%
+  unnest_tokens(word, text)
+word_counts <- by_chapter_word %>%
+  anti_join(stop_words) %>%
+  count(document, word, sort = TRUE) %>%
+  ungroup()
+word_counts
