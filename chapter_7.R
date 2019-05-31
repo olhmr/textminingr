738401fb88539d7edad2be310face5b3e5fe4806
Olle Hammarström <olle.hammarstrom@gmail.com>
2019-05-31 18:57:19 +0100

    Finished 7.2

--- chapter_7.R
+++ chapter_7.R
@@ -13,3 +13,39 @@ tweets <- bind_rows(tweets_julia %>%
 ggplot(data = tweets, aes(x = timestamp, fill = person)) +
   geom_histogram(position = "identity", bins = 20, show.legend = FALSE) +
   facet_wrap(~person, ncol = 1)
+
+library(tidytext)
+library(stringr)
+remove_reg <- "&amp;|&lt;|&gt;" # I guess this catches formatting artifacts
+tidy_tweets <- tweets %>%
+  filter(!str_detect(text, "^RT")) %>% # Remove re-tweets
+  mutate(text = str_remove_all(text, remove_reg)) %>%
+  unnest_tokens(word, text, token = "tweets") %>%
+  filter(!word %in% stop_words$word,
+         !word %in% str_remove_all(stop_words$word, "'"),
+         str_detect(word, "[a-z]"))
+# This doesn't remove links, though
+
+frequency <- tidy_tweets %>%
+  group_by(person) %>%
+  count(word, sort = TRUE) %>%
+  left_join(tidy_tweets %>%
+              group_by(person) %>%
+              summarise(total = n())) %>%
+  mutate(freq = n / total)
+frequency
+
+library(tidyr)
+frequency <- frequency %>%
+  select(person, word, freq) %>%
+  spread(person, freq) %>%
+  arrange(Julia, David)
+frequency
+
+library(scales)
+ggplot(data = frequency, aes(x = Julia, y = David)) +
+  geom_jitter(alpha = 0.1, size = 2.5, width = 0.25, height = 0.25) +
+  geom_text(aes(label = word), check_overlap = TRUE, vjust = 1.5) +
+  scale_x_log10(labels = percent_format()) +
+  scale_y_log10(labels = percent_format()) +
+  geom_abline(color = "red")
