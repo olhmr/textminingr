1b948e63608e5dd7c3b0a6553308e9b701995c28
Olle Hammarström <olle.hammarstrom@gmail.com>
2019-06-01 11:13:01 +0100

    Finished 7.3

--- chapter_7.R
+++ chapter_7.R
@@ -49,3 +49,32 @@ ggplot(data = frequency, aes(x = Julia, y = David)) +
   scale_x_log10(labels = percent_format()) +
   scale_y_log10(labels = percent_format()) +
   geom_abline(color = "red")
+
+tidy_tweets <- tidy_tweets %>% 
+  filter(timestamp >= as.Date("2016-01-01"),
+         timestamp < as.Date("2017-01-01"))
+
+word_ratios <- tidy_tweets %>%
+  filter(!grepl(pattern = "^@", x = word)) %>%
+  count(word, person) %>%
+  group_by(word) %>%
+  filter(sum(n) >= 10) %>%
+  ungroup() %>%
+  spread(person, n, fill = 0) %>%
+  mutate_if(is.numeric, funs((. + 1) / (sum(.) + 1))) %>%
+  mutate(logratio = log(David / Julia)) %>%
+  arrange(desc(logratio))
+
+word_ratios %>% 
+  arrange(abs(logratio))
+
+word_ratios %>%
+  group_by(logratio < 0) %>% # Group by positive versus negative ratio (with log cases where Julia > David will be negative)
+  top_n(15, abs(logratio)) %>%
+  ungroup() %>%
+  mutate(word = reorder(word, logratio)) %>%
+  ggplot(aes(x = word, y = logratio, fill = logratio < 0)) + 
+  geom_col(show.legend = FALSE) +
+  coord_flip() +
+  ylab("log odds ratio (David / Julia)") +
+  scale_fill_discrete(name = "", labels = c("David", "Julia"))
