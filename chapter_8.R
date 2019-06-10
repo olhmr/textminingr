faa37540f3873083fd3fc70f2a71044673656474
Olle <olle.hammarstrom@gmail.com>
2019-06-10 16:39:45 +0100

    Started chapter 8

---
+++ chapter_8.R
@@ -0,0 +1,9 @@
+### Chapter 8
+
+library(jsonlite)
+metadata <- fromJSON("https://data.nasa.gov/data.json")
+names(metadata$dataset)
+
+class(metadata$dataset$title)
+class(metadata$dataset$description)
+class(metadata$dataset$keyword)
