5ee36a92d56d276707760f14134291f24adcb2fe
Olle Hammarström <olle.hammarstrom@gmail.com>
2019-06-13 21:15:17 +0100

    Finished 8.1.1

--- chapter_8.R
+++ chapter_8.R
@@ -7,3 +7,31 @@ names(metadata$dataset)
 class(metadata$dataset$title)
 class(metadata$dataset$description)
 class(metadata$dataset$keyword)
+
+library(dplyr)
+nasa_title <- tibble(id = metadata$dataset$identifier, # The version in the text is not working
+                     title = metadata$dataset$title)
+nasa_title
+
+nasa_desc <- tibble(id = metadata$dataset$identifier,
+                    desc = metadata$dataset$description)
+nasa_desc %>%
+  select(desc) %>%
+  sample_n(5)
+
+library(tidyr)
+nasa_keyword <- tibble(id = metadata$dataset$identifier,
+                       keyword = metadata$dataset$keyword) %>% 
+  unnest(keyword)
+nasa_keyword
+
+library(tidytext)
+nasa_title <- nasa_title %>%
+  unnest_tokens(word, title) %>%
+  anti_join(stop_words)
+nasa_desc <- nasa_desc %>%
+  unnest_tokens(word, desc) %>%
+  anti_join(stop_words)
+
+nasa_title
+nasa_desc
